e9f161f990cc778952a73f3b436c4481c56b20bb
noorun_nazira <noorun.nazira@concentrix.com>
2018-12-03 15:14:22 +0530

    Updated Camel casing mistake.

--- data/src/main/java/com/example/data/loginsharedpreference/LoginSharedPreference.kt
+++ data/src/main/java/com/example/data/loginsharedpreference/LoginSharedPreference.kt
@@ -1,7 +1,6 @@
 package com.example.data.loginsharedpreference
 
 import android.content.Context
-import android.util.Log
 import com.example.domain.model.LoginCredential
 import com.example.domain.repository.LoginRepository
 import io.reactivex.Observable
@@ -15,11 +14,11 @@ import io.reactivex.Observable
  * and pass the login details to SharedPreference
  */
 class LoginSharedPreference : LoginRepository {
-    override fun storeCredential(context: Context, logincredential: LoginCredential): Observable<LoginCredential> {
+    override fun storeCredential(context: Context, loginCreds: LoginCredential): Observable<LoginCredential> {
         SharedPreferences.init(context)
-        SharedPreferences.write(SharedPreferences.EMAIL, logincredential.email)
-        SharedPreferences.write(SharedPreferences.PASSWORD, logincredential.password)
+        SharedPreferences.write(SharedPreferences.EMAIL, loginCreds.email)
+        SharedPreferences.write(SharedPreferences.PASSWORD, loginCreds.password)
 
-        return Observable.just(logincredential)
+        return Observable.just(loginCreds)
     }
 }
